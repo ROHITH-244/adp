0f21f87566a5da220004f4b166e902e0c64a2baa
ROHITH-244 <140065510+ROHITH-244@users.noreply.github.com>
2024-09-26 08:23:26 +0530

    Add files via upload

---
+++ JAVASCRIPT/employee.js
@@ -0,0 +1,9 @@
+// Example data to populate the boxes
+let shortlistedCandidates = 50;
+let totalJobPostings = 10;
+let totalApplications = 200;
+
+// Populate the box with the data
+document.getElementById("shortlisted-count").textContent = shortlistedCandidates;
+document.getElementById("job-posting-count").textContent = totalJobPostings;
+document.getElementById("application-count").textContent = totalApplications;
