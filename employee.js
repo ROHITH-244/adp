0ce035a7c387585cd563739bd4197e707dd4c674
ROHITH-244 <140065510+ROHITH-244@users.noreply.github.com>
2024-09-26 08:14:04 +0530

    Delete employee.js

--- employee.js
+++
@@ -1,9 +0,0 @@
-// Example data to populate the boxes
-let shortlistedCandidates = 50;
-let totalJobPostings = 10;
-let totalApplications = 200;
-
-// Populate the box with the data
-document.getElementById("shortlisted-count").textContent = shortlistedCandidates;
-document.getElementById("job-posting-count").textContent = totalJobPostings;
-document.getElementById("application-count").textContent = totalApplications;
